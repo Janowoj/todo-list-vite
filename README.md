4d0b3bdbd1af569201b71bf19cdb4a4478b129ec
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2024-02-06 01:40:29 +0100

    first commit: adding todos without components

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + Vite
+
+This is a simple todo list app built with React and Vite.
+Functionality includes adding, deleting, and marking tasks as complete.
+Also added a date of creation of the task using getDay() and getMonth() methods.
+
+For styling used Tailwind CSS.
---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import './App.css';
+import { useState } from 'react';
+
+const todoList = [
+  {
+    id: 1,
+    task: 'wyrzucić śmieci',
+  },
+  {
+    id: 2,
+    task: 'zrobić zakupy',
+  },
+];
+
+function App() {
+  const [isInputShown, setIsInputShown] = useState(false);
+  const [taskList, setTaskList] = useState(todoList);
+  const [inputValue, setInputValue] = useState('');
+
+  const todoElement = taskList.map((item) => (
+    <ul key={item.id} className="m-2 flex flex-row justify-between">
+      <li className="mt-2">{`> ${item.task}`}</li>
+      <div>
+        <button
+          onClick={handleCheck}
+          className="p-2 border-2 border-teal-600 rounded-md"
+        >
+          Zrobione!
+        </button>
+        <button
+          onClick={() => handleRemove(item.id)}
+          className="p-2 ml-2 border-2 border-teal-600 rounded-md"
+        >
+          Usuń
+        </button>
+      </div>
+    </ul>
+  ));
+
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleSubmit() {
+    setIsInputShown(false);
+    setTaskList((prevList) => [
+      ...prevList,
+      { id: prevList.length + 1, task: inputValue },
+    ]);
+    setInputValue('');
+  }
+
+  function handleRemove(id) {
+    setTaskList((prevList) => prevList.filter((item) => item.id !== id));
+  }
+
+  function handleCheck(e) {
+    e.target.parentElement.parentElement.style.textDecoration = 'line-through';
+  }
+
+  return (
+    <>
+      <div className="p-4 border-4 border-black w-96 rounded-lg bg-white">
+        <div className="flex flex-row justify-between">
+          <h1 className="mb-2 font-bold self-center">
+            DO ZROBIENIA{' '}
+            {`${
+              new Date().getDate() > 9
+                ? new Date().getDate()
+                : '0' + new Date().getDate()
+            }/${
+              new Date().getMonth().length > 1
+                ? new Date().getMonth()
+                : '0' + new Date().getMonth()
+            }/${new Date().getFullYear()}`}
+            :
+          </h1>
+          {!isInputShown && (
+            <button
+              onClick={() => setIsInputShown(true)}
+              className="border-2 border-teal-600 w-8 h-8 rounded"
+            >
+              +
+            </button>
+          )}
+        </div>
+        {/* <hr className="my-2 bg-slate-900 h-1" /> */}
+        <div className="flex flex-col">
+          <label htmlFor="text-input" className="font-medium">
+            ilość zadań: {taskList.length}
+          </label>
+          <div className="flex justify-between my-2">
+            {isInputShown && (
+              <>
+                <input
+                  onChange={handleInput}
+                  type="text"
+                  className="border-2 border-black h-8 rounded"
+                  id="text-input"
+                  value={inputValue}
+                />
+
+                <button
+                  onClick={handleSubmit}
+                  className="p-2 border-2 border-teal-600 rounded-md"
+                >
+                  Dodaj
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+
+        <hr className="bg-slate-900 h-1" />
+        {todoElement}
+      </div>
+    </>
+  );
+}
+
+export default App;
