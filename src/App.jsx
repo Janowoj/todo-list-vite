75ac7eaf6ddd6a6600dcbd74b6c85f02c33be6aa
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2024-02-06 02:11:40 +0100

    added a little bit styling

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   const todoElement = taskList.map((item) => (
     <ul key={item.id} className="m-2 flex flex-row justify-between">
-      <li className="mt-2">{`> ${item.task}`}</li>
+      <li className="mx-4 my-2 font-semibold">{`> ${item.task}`}</li>
       <div>
         <button
           onClick={handleCheck}
@@ -59,62 +59,60 @@ function App() {
   }
 
   return (
-    <>
-      <div className="p-4 border-4 border-black w-96 rounded-lg bg-white">
-        <div className="flex flex-row justify-between">
-          <h1 className="mb-2 font-bold self-center">
-            DO ZROBIENIA{' '}
-            {`${
-              new Date().getDate() > 9
-                ? new Date().getDate()
-                : '0' + new Date().getDate()
-            }/${
-              new Date().getMonth().length > 1
-                ? new Date().getMonth()
-                : '0' + new Date().getMonth()
-            }/${new Date().getFullYear()}`}
-            :
-          </h1>
-          {!isInputShown && (
-            <button
-              onClick={() => setIsInputShown(true)}
-              className="border-2 border-teal-600 w-8 h-8 rounded"
-            >
-              +
-            </button>
+    <div className="p-4 border-4 border-black rounded-lg bg-white">
+      <div className="flex flex-row justify-between">
+        <h1 className="mb-2 font-bold self-center">
+          DO ZROBIENIA{' '}
+          {`${
+            new Date().getDate() > 9
+              ? new Date().getDate()
+              : '0' + new Date().getDate()
+          }/${
+            new Date().getMonth().length > 1
+              ? new Date().getMonth()
+              : '0' + new Date().getMonth()
+          }/${new Date().getFullYear()}`}
+          :
+        </h1>
+        {!isInputShown && (
+          <button
+            onClick={() => setIsInputShown(true)}
+            className="border-2 border-teal-600 w-8 h-8 rounded"
+          >
+            +
+          </button>
+        )}
+      </div>
+      {/* <hr className="my-2 bg-slate-900 h-1" /> */}
+      <div className="flex flex-col">
+        <label htmlFor="text-input" className="font-semibold">
+          ilość zadań: {taskList.length}
+        </label>
+        <div className="flex justify-between my-2">
+          {isInputShown && (
+            <>
+              <input
+                onChange={handleInput}
+                type="text"
+                className="border-2 border-black h-8 rounded"
+                id="text-input"
+                value={inputValue}
+              />
+
+              <button
+                onClick={handleSubmit}
+                className="mr-2 p-2 border-2 border-teal-600 rounded-md"
+              >
+                Dodaj
+              </button>
+            </>
           )}
         </div>
-        {/* <hr className="my-2 bg-slate-900 h-1" /> */}
-        <div className="flex flex-col">
-          <label htmlFor="text-input" className="font-medium">
-            ilość zadań: {taskList.length}
-          </label>
-          <div className="flex justify-between my-2">
-            {isInputShown && (
-              <>
-                <input
-                  onChange={handleInput}
-                  type="text"
-                  className="border-2 border-black h-8 rounded"
-                  id="text-input"
-                  value={inputValue}
-                />
-
-                <button
-                  onClick={handleSubmit}
-                  className="p-2 border-2 border-teal-600 rounded-md"
-                >
-                  Dodaj
-                </button>
-              </>
-            )}
-          </div>
-        </div>
-
-        <hr className="bg-slate-900 h-1" />
-        {todoElement}
       </div>
-    </>
+
+      <hr className="bg-slate-900 h-1" />
+      {todoElement}
+    </div>
   );
 }
 
